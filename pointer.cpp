b15c5153c5911cc40baf29cbf3e3c3d64ca3edf8
abdullahchheema <abdullahimran1677@gmail.com>
2025-09-18 23:30:09 -0700

    Analyzing Pointer Behaviour

--- pointer.cpp
+++ pointer.cpp
@@ -5,12 +5,25 @@ using namespace std;
 void analyze_pointer(int* ptr) {
 	cout << "Address of number: " << ptr << endl;
 	cout << "Value at address: " << *ptr << endl;
+	cout << endl;
 }  
 
 int main() {
+	// Part 1
 	int number = 2005;
+	//analyze_pointer(&number);
+
+	// Part 2
 	
-	analyze_pointer(&number);
+	int iValue = 2025;
+	analyze_pointer(&iValue);
+
+	int* heap = new int;
+	*heap = 1245;
+	analyze_pointer(heap);
+
+	
+	
 
 
 
